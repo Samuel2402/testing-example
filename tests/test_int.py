2eea1589d8543e02a52174c72e31ab7c98e71811
Samuel <Sam.castle02@gmail.com>
2021-06-17 13:15:51 +0000

    new commit

--- tests/test_int.py
+++ tests/test_int.py
@@ -60,4 +60,12 @@ class TestAdd(TestBase):
             self.assertNotEqual(entry, None)
 
     def test_empty_validation(self):
+        self.submit_input('')
+        self.assertIn(url_for('index'), self.driver.current_url)
+
+        text = self.driver.find_element_by_xpath('/html/body/div/i').text
+        self.assertIn("The name field can't be empty!", text)
+
+        entries = Games.query.all()
+        self.assertEqual(len(entries), 0) # database should be empty
         pass
